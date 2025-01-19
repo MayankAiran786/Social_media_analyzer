1fb267fa65335bb316f50c8ef793833ffc1d992a
MayankAiran786 <mayank83520@gmail.com>
2025-01-20 00:34:20 +0530

    Hello

--- app.py
+++ app.py
@@ -89,3 +89,6 @@ def upload_file():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
